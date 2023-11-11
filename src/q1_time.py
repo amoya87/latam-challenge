fa886c863047e26ab7dbfa2fe06476e1263eddb2
amoya87 <albertomoya87@gmail.com>
2023-11-11 05:33:32 -0300

    feat(dates): Implementation for q1_time

--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,40 @@
-from typing import List, Tuple
+import json
+from collections import Counter, defaultdict
 from datetime import datetime
+from typing import List, Tuple
+
 
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    # Create a dictionary to count objects by date and name
+    date_name_count = defaultdict(Counter)
+    # Create a dictionary to count objects by timestamps
+    date_count = defaultdict(int)
+
+    with open(file_path, encoding="utf-8") as f:
+        for line in f:
+            try:
+                doc = json.loads(line)
+                date = datetime.fromisoformat(doc["date"]).date()
+                timestamp = int(
+                    datetime.combine(date, datetime.min.time()).timestamp()
+                )
+                name = doc["user"]["username"]
+                date_name_count[timestamp][name] += 1
+                date_count[timestamp] += 1
+            except (json.JSONDecodeError, KeyError) as e:
+                # Handle JSON decoding errors or missing keys
+                print(f"Error processing line: {line.strip()}. Error: {e}")
+
+    # Find the 10 dates with the most objects
+    top_10_dates = sorted(
+        date_count, key=lambda dat: date_count[dat], reverse=True
+    )[:10]
+
+    # For each date in the top 10, find the most common name
+    result = []
+    for date in top_10_dates:
+        common_name = date_name_count[date].most_common(1)[0][0]
+        date_time = datetime.utcfromtimestamp(date)
+        result.append((date_time.date(), common_name))
+
+    return result
