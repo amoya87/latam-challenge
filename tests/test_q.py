d3586ea5bd8da4278ea673e398ef055df25791b6
amoya87 <albertomoya87@gmail.com>
2023-11-11 03:50:15 -0300

    feat(tests): Add q_1 test

--- tests/test_q.py
+++ tests/test_q.py
@@ -1,7 +1,10 @@
+import datetime
 import json
 import os
 import sys
 import unittest
+
+from src import q1_memory
 
 # Get the full path to the 'src' directory
 src_path = os.path.abspath(
@@ -18,57 +21,57 @@ class TestSuit(unittest.TestCase):
         # Create a test JSON file with dummy data
         test_data = [
             {
-                "date": None,
+                "date": "2021-02-24T09:22:11+00:00",
                 "content": None,
-                "user": None,
+                "user": {"username": "Alberto"},
                 "mentionedUsers": None,
             },
             {
-                "date": None,
+                "date": "2021-02-24T09:22:11+00:00",
                 "content": None,
-                "user": None,
+                "user": {"username": "Moya"},
                 "mentionedUsers": None,
             },
             {
-                "date": None,
+                "date": "2021-02-24T09:22:11+00:00",
                 "content": None,
-                "user": None,
+                "user": {"username": "Loustaunau"},
                 "mentionedUsers": None,
             },
             {
-                "date": None,
+                "date": "2021-02-24T09:22:11+00:00",
                 "content": None,
-                "user": None,
+                "user": {"username": "Alberto"},
                 "mentionedUsers": None,
             },
             {
-                "date": None,
+                "date": "2021-02-25T09:22:08+00:00",
                 "content": None,
-                "user": None,
+                "user": {"username": "Moya"},
                 "mentionedUsers": None,
             },
             {
-                "date": None,
+                "date": "2021-02-25T09:22:09+00:00",
                 "content": None,
-                "user": None,
+                "user": {"username": "Moya"},
                 "mentionedUsers": None,
             },
             {
-                "date": None,
+                "date": "2021-02-25T09:22:10+00:00",
                 "content": None,
-                "user": None,
+                "user": {"username": "Moya"},
                 "mentionedUsers": None,
             },
             {
-                "date": None,
+                "date": "2021-02-25T09:22:11+00:00",
                 "content": None,
-                "user": None,
+                "user": {"username": "Alberto"},
                 "mentionedUsers": None,
             },
             {
-                "date": None,
+                "date": "2021-02-25T09:22:11+00:00",
                 "content": None,
-                "user": None,
+                "user": {"username": "Alberto"},
                 "mentionedUsers": None,
             },
         ]
@@ -78,7 +81,7 @@ class TestSuit(unittest.TestCase):
             for line in lines:
                 file.write(line + "\n")
 
-        self.expected_dates = []
+        self.expected_dates = ["2021-02-25", "2021-02-24"]
 
         self.expected_emojis = []
 
@@ -89,3 +92,17 @@ class TestSuit(unittest.TestCase):
         import os
 
         os.remove("test_data.json")
+
+    def test_q1(self):
+        # Call the function with the test JSON file
+        result = q1_memory.q1_memory("test_data.json")
+
+        # Checks if the result is a list of tuples with the correct format
+        for _date, name in result:
+            self.assertIsInstance(_date, datetime.date)
+            self.assertIsInstance(name, str)
+
+        # Check that the dates in the result are in the correct order
+        dates = [date.strftime("%Y-%m-%d") for date, _ in result]
+
+        self.assertEqual(dates, self.expected_dates)
